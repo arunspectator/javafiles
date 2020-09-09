812377ed493a7af0e8039afd092685dd382821ae
arunspectator <arunspectator@gmail.com>
2020-09-09 12:21:31 +0530

    adding student

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student{  
+	   
+	 int id=10;
+	 String name;  
+	
+	 public static void main(String args[]){  
+		 
+	 
+	  Student s1=new Student();  
+	  Student s2=new Student();
+	  //Printing values of the object
+	 
+	  System.out.println(s1.id);
+	  System.out.println(s2.id);
+	  System.out.println(s1.name);  
+	 }  
+	}  
