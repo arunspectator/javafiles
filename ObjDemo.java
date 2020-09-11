61658fc4ceaa4fc8564cd895a4cf66cf9ef33d36
arunspectator <arunspectator@gmail.com>
2020-09-11 10:03:22 +0530

    obj

---
+++ ObjDemo.java
@@ -0,0 +1,25 @@
+package oops;
+
+public class ObjDemo {
+	int a;
+	public static void main(String[] args) {
+		
+		ObjDemo o1,o2;
+		o1=new ObjDemo();
+		o2=new ObjDemo();
+		o1.a=100;
+		o2.a=100;
+		if(o1==o2) {
+			System.out.println("Both are equal");
+		}
+		else
+			System.out.println("Both are not equal");
+		o2=o1;
+		if(o1==o2) {
+			System.out.println("Both are equal");
+		}
+		else
+			System.out.println("Both are not equal");
+	}
+
+}
